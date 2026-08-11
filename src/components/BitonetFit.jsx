a0e62c12204f84520c19c4b307569250fb7dccf9
Gek2or <74296171+Gek2or@users.noreply.github.com>
2026-08-11 09:45:33 -0700

    Make Bitonet fit section more focused and editorial

--- src/components/BitonetFit.jsx
+++ src/components/BitonetFit.jsx
@@ -3,18 +3,23 @@ import { Building2, CheckCircle2 } from 'lucide-react'
 export default function BitonetFit({ t }) {
   return (
     <section className="contentSection sectionShell" id="bitonet">
-      <div className="bitonetHeader glassPanel">
-        <div className="bitonetMark"><Building2 size={28}/><span>BITONET OY</span></div>
+      <div className="sectionIntroGrid bitonetIntroGrid">
         <div>
+          <p className="sectionNumber">04</p>
           <p className="overline">{t.bitonetEyebrow}</p>
           <h2>{t.bitonetTitle}</h2>
+        </div>
+        <div className="bitonetStatement">
+          <Building2 size={26}/>
           <p>{t.bitonetIntro}</p>
         </div>
       </div>
-      <div className="fitGrid">
-        {t.fit.map(([title, text]) => (
-          <article className="fitCard" key={title}>
-            <CheckCircle2 size={22}/>
+
+      <div className="fitList">
+        {t.fit.map(([title, text], index) => (
+          <article className="fitRow" key={title}>
+            <span className="fitIndex">0{index + 1}</span>
+            <CheckCircle2 size={18}/>
             <h3>{title}</h3>
             <p>{text}</p>
           </article>
