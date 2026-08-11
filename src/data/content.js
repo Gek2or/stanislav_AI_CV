507d1b779003c2ba5508400997cdda023872291a
Gek2or <74296171+Gek2or@users.noreply.github.com>
2026-08-11 09:08:09 -0700

    Split multilingual content into language modules

---
+++ src/data/content.js
@@ -0,0 +1,13 @@
+import en from './en.js'
+import fi from './fi.js'
+import uk from './uk.js'
+import ru from './ru.js'
+
+export const languages = [
+  { id: 'en', label: 'EN' },
+  { id: 'fi', label: 'FI' },
+  { id: 'uk', label: 'UA' },
+  { id: 'ru', label: 'RU' },
+]
+
+export const content = { en, fi, uk, ru }
